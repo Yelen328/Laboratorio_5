61a866415aa45536b086dded34b899ec52e99910
Yelen328 <cot231076@uvg.edu.gt>
2025-04-10 22:34:23 -0600

    prelab
    Inicialización del prelaboratorio

---
+++ Prelaboratorio_5/Prelaboratorio_5/main.c
@@ -0,0 +1,18 @@
+/*
+ * Prelaboratorio_5.c
+ *
+ * Created: 10/4/2025 22:32:40
+ * Author : yelen
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
